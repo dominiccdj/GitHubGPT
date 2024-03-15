ee65534915ca2d1c11b98c102d38c33ab674aafe
Dominik Jurkovic <d.jurkovic@reply.de>
2024-03-15 17:21:53 +0100

    Optimize the app

--- index.js
+++ index.js
@@ -1,21 +1,65 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Probot} app
- */
 module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
 
+  const axios = require('axios');
+
+  app.log.info("App is up and running!");
+
+  // Issue opened
   app.on("issues.opened", async (context) => {
+
+    const commenter = context.payload.issue.user.login;
+
+    const body = `Hello @${commenter}! 👋 \n\n I'm a friendly AI Bot, designed to chat with you in this repository. \n If you have any questions or need help with something, feel free to ask, and I'll do my best to assist you. \n Have a great day! \n\n Best regards,\n The GitHubGPT AI Bot 🤖`;
+
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: body,
     });
     return context.octokit.issues.createComment(issueComment);
   });
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  // Issue comment created
+  app.on("issue_comment.created", async (context) => {
+
+    if (context.payload.comment.user.type === 'User') {
+
+      const message = context.payload.comment.body;
+
+      const payload = {
+        "model": "Nous-Hermes2",
+        "max_tokens": 500,
+        "messages": [
+          {
+            "role": "user",
+            "content": message
+          }
+        ]
+      };
+
+      axios.post('http://localhost:4891/v1/chat/completions', payload)
+        .then(response => {
+          const responseData = response.data;
+
+          const newObject = {
+            choiceMessages: responseData.choices.map(choice => choice.message),
+            createdTimestamp: responseData.created,
+            requestId: responseData.id,
+            modelName: responseData.model,
+            objectType: responseData.object,
+            tokenUsage: responseData.usage
+          };
+
+          const issueComment = context.issue({
+            body: newObject.choiceMessages[0].content,
+          });
+
+          context.octokit.issues.createComment(issueComment);
+
+        })
+        .catch(error => {
+          console.error('An error occurred! ', error);
+        });
+    }
+  });
+
 };
